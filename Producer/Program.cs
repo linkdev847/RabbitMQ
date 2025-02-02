c5a00527fe68aecad07a178a95178ab0e715bd7a
Ashraf Shaker <Ashraf.Shaker@linkdev.com>
2025-02-02 14:55:35 +0200

    Add project files.

---
+++ Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+
+
+const string message = "Hello World!33";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
